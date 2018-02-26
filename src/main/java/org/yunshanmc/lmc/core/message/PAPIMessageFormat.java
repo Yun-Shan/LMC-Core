a4294ffeb3c603d5b1fbcbdec824ee3c17fb8979
Yun-Shan <yunshan21st@qq.com>
2018-02-26 21:08:00 +0800

    加入PlaceholderAPI插件启用判断

--- src/main/java/org/yunshanmc/lmc/core/message/PAPIMessageFormat.java
+++ src/main/java/org/yunshanmc/lmc/core/message/PAPIMessageFormat.java
@@ -5,18 +5,31 @@
 package org.yunshanmc.lmc.core.message;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.yunshanmc.lmc.core.utils.PlatformUtils;
 
 /**
  * //TODO 注释
  */
 public class PAPIMessageFormat extends DefaultMessageFormat {
 
-    public PAPIMessageFormat(MessageContext context) { super(context); }
+    private static boolean isPlaceholderAPIEnable = false;
+
+    static {
+        if (PlatformUtils.isBukkit() && Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            isPlaceholderAPIEnable = true;
+        }
+    }
+
+    public PAPIMessageFormat(MessageContext context) {
+        super(context);
+    }
 
     @Override
     public String format(Player player, String msg, Object... args) {
-        return PlaceholderAPI.setPlaceholders(player, super.format(player, msg, args));
+        msg = super.format(player, msg, args);
+        if (isPlaceholderAPIEnable) msg = PlaceholderAPI.setPlaceholders(player, msg);
+        return msg;
     }
 }
