67c48293835cfa3a86a50a96436f93a5e14a79b7
Yun Shan <yunshan21st@qq.com>
2020-09-11 16:53:21 +0800

    jacoco

---
+++ bungeecord/src/test/java/org/yunshanmc/lmc/core/bungee/TempTest.java
@@ -0,0 +1,15 @@
+package org.yunshanmc.lmc.core.bungee;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 用于避免因为没有测试用例导致无法生成测试覆盖率报告的错误，
+ * 这边写了测试用例之后就可以删了
+ */
+public class TempTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
